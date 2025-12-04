293fa9983e27f7b03a11ac3b91245e03ea8bb744
Dav1dVaca <davacagu@uide.edu.ec>
2025-12-04 13:18:10 +0000

    Mi segundo script

---
+++ script2.sh
@@ -0,0 +1,11 @@
+# uname -a: muestra información completa del sistema operativo y el kernel
+uname -a
+
+# lscpu: muestra información detallada del procesador (arquitectura, núcleos, hilos, etc.)
+lscpu
+
+# free -h: muestra la memoria RAM disponible y usada; -h la muestra en formato legible (MB/GB)
+free -h
+
+# df -h: muestra el uso del disco duro; -h lo presenta en formato legible (MB/GB)
+df -h
